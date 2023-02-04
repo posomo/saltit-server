2d778d00b7447fb4172ed85b420ad2429975ff7a
vcho1958 <gusckd57@gmail.com>
2023-02-04 21:13:13 +0900

    init

---
+++ src/main/java/com/posomo/saltit/SaltitServerApplication.java
@@ -0,0 +1,13 @@
+package com.posomo.saltit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaltitServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posomo/saltit/SaltitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posomo.saltit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
