5742eac3b2a0e4b0e39003afb736e3f2d1fdde29
KTH <johan1103@naver.com>
2023-03-10 18:01:45 +0900

    [feat] 레스토랑 DTO정보를 Object로 받은 후 DTO로 매핑해서 반환하는 메서드 구현
    jpa에서 Native쿼리로 결과를 반환 했을 때, DTO로 자동 매핑해주는 기능을 찾아보았으나, @resultsetmapping과 같은 엔티티에 코드가 추가되는 방식 보다는 직접 매핑해주는 방법이 더 직관적이라고 생각했습니다.

--- src/main/java/com/posomo/saltit/service/RestaurantService.java
+++ src/main/java/com/posomo/saltit/service/RestaurantService.java
@@ -1,12 +1,37 @@
 package com.posomo.saltit.service;
+import com.posomo.saltit.domain.restaurant.dto.RestaurantFilterRequest;
+import com.posomo.saltit.domain.restaurant.dto.RestaurantSummary;
+import com.posomo.saltit.domain.restaurant.entity.RestaurantMenu;
 import com.posomo.saltit.respository.RestaurantRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
+    public Slice<RestaurantSummary> getRestaurantSummaries(RestaurantFilterRequest filterRequest){
+        PageRequest pageRequest = PageRequest.of(filterRequest.getPage(), filterRequest.getSize());
+        Slice<Object[]> resultObjects = restaurantRepository.findRestaurantByFilter(filterRequest.getMaxPrice(), filterRequest.getFoodTypeName(),
+                filterRequest.getUserLongitude(),filterRequest.getUserLatitude(),filterRequest.getMaxDistance()
+        ,pageRequest);
+        return getRestaurantSummaryFromObjects(resultObjects);
+    }
+    private Slice<RestaurantSummary> getRestaurantSummaryFromObjects(Slice<Object[]> restaurantSummaries){
+        return restaurantSummaries.map(objects-> RestaurantSummary.create(
+            objects[0] == null ? null : ((String)objects[0]),
+            objects[1] == null ? null : ((String)objects[1]),
+            objects[2] == null ? null : ((Integer)objects[2]),
+            objects[3] == null ? null : ((Integer)objects[3]),
+            objects[4] == null ? null : ((String)objects[4]),
+            objects[5] == null ? null : ((String)objects[5]),
+            objects[6] == null ? null : ((Double)objects[6])
+        ));
+    }
 }
