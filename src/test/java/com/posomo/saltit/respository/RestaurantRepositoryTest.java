86fde1cd5f511f4fa45e4ecd4465e851048b003e
inhyeok <alzpqm@naver.com>
2023-04-25 00:01:48 +0900

    Test: 식당 세부 정보 조회 API 테스트

--- src/test/java/com/posomo/saltit/respository/RestaurantRepositoryTest.java
+++ src/test/java/com/posomo/saltit/respository/RestaurantRepositoryTest.java
@@ -1,4 +1,89 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.posomo.saltit.respository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.posomo.saltit.domain.restaurant.entity.Restaurant;
+import com.posomo.saltit.domain.restaurant.entity.RestaurantMenu;
+
+import jakarta.persistence.EntityManager;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class RestaurantRepositoryTest {
-  
+
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	@Autowired
+	private EntityManager entityManager;
+
+	@Nested
+	class findByIdWithMenus {
+		@Test
+		@DisplayName("정상흐름")
+		void ok() {
+			//given
+			List<RestaurantMenu> menus = new ArrayList<>();
+			Restaurant restaurant = Restaurant.create(1L, null, "테스트 가게", null, 100, null, null, menus, null, null, null);
+			menus.add(new RestaurantMenu(null, restaurant, null, null, null, null, true));
+			menus.add(new RestaurantMenu(null, restaurant, null, null, null, null, true));
+			menus.add(new RestaurantMenu(null, restaurant, null, null, null, null, true));
+			menus.add(new RestaurantMenu(null, restaurant, null, null, null, null, true));
+			menus.add(new RestaurantMenu(null, restaurant, null, null, null, null, true));
+			menus.add(new RestaurantMenu(null, restaurant, null, null, null, null, true));
+
+			//when
+			Restaurant savedRestaurant = restaurantRepository.save(restaurant);
+			entityManager.flush();
+			entityManager.clear();
+
+			//then
+			Restaurant findRestaurant = restaurantRepository.findByIdWithMenus(savedRestaurant.getId()).get();
+			assertThat(findRestaurant.getId()).isEqualTo(savedRestaurant.getId());
+			assertThat(findRestaurant.getName()).isEqualTo(savedRestaurant.getName());
+			assertThat(findRestaurant.getScore()).isEqualTo(savedRestaurant.getScore());
+			assertThat(findRestaurant.getMenus()).hasSize(menus.size());
+		}
+
+		@Test
+		@DisplayName("Menu가 0개일때")
+		void edge1() {
+			//given
+			List<RestaurantMenu> menus = new ArrayList<>();
+			Restaurant restaurant = Restaurant.create(1L, null, "테스트 가게", null, 100, null, null, menus, null, null, null);
+
+			//when
+			Restaurant savedRestaurant = restaurantRepository.save(restaurant);
+			entityManager.flush();
+			entityManager.clear();
+
+			//then
+			Restaurant findRestaurant = restaurantRepository.findByIdWithMenus(savedRestaurant.getId()).get();
+			assertThat(findRestaurant.getId()).isEqualTo(savedRestaurant.getId());
+			assertThat(findRestaurant.getName()).isEqualTo(savedRestaurant.getName());
+			assertThat(findRestaurant.getScore()).isEqualTo(savedRestaurant.getScore());
+			assertThat(findRestaurant.getMenus()).isEmpty();
+		}
+
+		@Test
+		@DisplayName("식당이 없을 때")
+		void edge2() {
+			//given
+			//when
+			//then
+			Optional<Restaurant> findRestaurant = restaurantRepository.findByIdWithMenus(1L);
+			assertThat(findRestaurant).isEmpty();
+		}
+	}
 }
