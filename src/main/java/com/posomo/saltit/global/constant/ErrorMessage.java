31ecaca57db4aa5e30b94b17ff64b1c6f09d4993
inhyeok <alzpqm@naver.com>
2023-04-28 20:18:16 +0900

    refactor: 상수 클래스의 생성자 제한
    상수 클래스는 Static 필드만 가지므로, 생성자를 사용하지 못하게 막아둔다.

--- src/main/java/com/posomo/saltit/global/constant/ErrorMessage.java
+++ src/main/java/com/posomo/saltit/global/constant/ErrorMessage.java
@@ -1,6 +1,9 @@
 package com.posomo.saltit.global.constant;
 
 public class ErrorMessage {
+	private ErrorMessage() {
+	}
+
 	public static final String UNKNOWN_ERROR = "알 수 없는 에러가 발생했습니다.";
 	public static final String RECORD_NOT_FOUND = "해당 데이터를 찾을 수 없습니다.";
 }
