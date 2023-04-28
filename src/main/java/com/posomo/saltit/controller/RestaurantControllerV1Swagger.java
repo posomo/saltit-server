244855711f126f04270b00765caa3cbefd9601de
inhyeok <alzpqm@naver.com>
2023-04-28 20:24:55 +0900

    style: unwrapping container annotation
    Java8 이후로 동일한 주석 여러개를 달 때, container를 사용하지 않고 반복적으로 annotation을 작성하면 되도록 변경되었다.

--- src/main/java/com/posomo/saltit/controller/RestaurantControllerV1Swagger.java
+++ src/main/java/com/posomo/saltit/controller/RestaurantControllerV1Swagger.java
@@ -12,25 +12,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 public interface RestaurantControllerV1Swagger {
 	@Operation(summary = "home 화면, 식당 요약 정보 제공용 api", description = "식당의 메인 이미지 주소, 페이지 정보," +
 		"식당 요약 리스트를 제공합니다.")
-	@ApiResponses({
-		@ApiResponse(responseCode = "200", description = "정상 응답",
-			content = @Content(schema = @Schema(implementation = RestaurantSummaryResponse.class))),
-		@ApiResponse(responseCode = "400", description = "유저 위도 경도 정보, 페이지 번호, 페이지 크기 중 하나라도 null일 때",
-			content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-	})
+	@ApiResponse(responseCode = "200", description = "정상 응답", content = @Content(schema = @Schema(implementation = RestaurantSummaryResponse.class)))
+	@ApiResponse(responseCode = "400", description = "유저 위도 경도 정보, 페이지 번호, 페이지 크기 중 하나라도 null일 때", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
 	RestaurantSummaryResponse getRestaurantSummaries(@RequestBody RestaurantFilterRequest filterRequest);
 
 	@Operation(summary = "식당 세부 정보 조회 api")
-	@ApiResponses({
-		@ApiResponse(responseCode = "200", description = "정상 응답",
-			content = @Content(schema = @Schema(implementation = RestaurantDetailResponse.class))),
-		@ApiResponse(responseCode = "400", description = "잘못된 요청",
-			content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-	})
+	@ApiResponse(responseCode = "200", description = "정상 응답", content = @Content(schema = @Schema(implementation = RestaurantDetailResponse.class)))
+	@ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
 	RestaurantDetailResponse getRestaurantDetail(@PathVariable("restaurantId") Integer restaurantId);
 }
