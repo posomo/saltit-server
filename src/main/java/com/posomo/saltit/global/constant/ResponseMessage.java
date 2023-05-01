e4834e58d4f5634d9afe4688ea825825d146ba6d
inhyeok <alzpqm@naver.com>
2023-05-01 14:28:46 +0900

    style: 줄바꿈 추가

--- src/main/java/com/posomo/saltit/global/constant/ResponseMessage.java
+++ src/main/java/com/posomo/saltit/global/constant/ResponseMessage.java
@@ -3,6 +3,7 @@ package com.posomo.saltit.global.constant;
 public class ResponseMessage {
 	private ResponseMessage() {
 	}
+
 	public static final String UNKNOWN_ERROR = "서버 에러가 발생했습니다. 개발팀으로 문의 주세요.";
 	public static final String RECODE_NOT_FOUND = "해당 레코드는 존재하지 않습니다.";
 	public static final String MISMATCH_PARAM = "파라미터의 형식이 잘못되었습니다.";
