1f3b3b6a807453cc882ab1dd58439c97cfe8e35f
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2023-05-08 14:58:31 +0900

    Fix: Swagger의 요청을 보내는 위치를 yml 파일을 통해 설정할 수 있도록 변경

--- src/main/java/com/posomo/saltit/swagger/SwaggerConfig.java
+++ src/main/java/com/posomo/saltit/swagger/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package com.posomo.saltit.swagger;
 
+import java.util.List;
+
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 // SwaggerConfig.java
@@ -20,6 +25,9 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 public class SwaggerConfig {
 
+	@Value("${server.nginx-url}")
+	private String nginxUrl;
+
 	@Bean
 	public GroupedOpenApi chatOpenApi() {
 		String[] paths = {"/**"};
@@ -28,5 +36,13 @@ public class SwaggerConfig {
 			.group("saltit api v1")
 			.pathsToMatch(paths)
 			.build();
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		Server server = new Server();
+		server.setUrl(nginxUrl);
+		server.description("현재 Swagger가 요청을 보내는 위치");
+		return new OpenAPI().servers(List.of(server));
 	}
 }
