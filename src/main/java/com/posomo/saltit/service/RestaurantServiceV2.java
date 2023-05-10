de1f595151594be456665ddbff00af3c2c8523b0
jhbaik1501 <jhbaik1501@gmail.com>
2023-05-10 23:55:16 +0900

    Feat: RestaurantServiceV2.java

---
+++ src/main/java/com/posomo/saltit/service/RestaurantServiceV2.java
@@ -0,0 +1,43 @@
+package com.posomo.saltit.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+import com.posomo.saltit.domain.exception.NoRecordException;
+import com.posomo.saltit.domain.restaurant.dto.RestaurantDetailResponse;
+import com.posomo.saltit.domain.restaurant.dto.RestaurantFilterRequest;
+import com.posomo.saltit.domain.restaurant.dto.RestaurantSummary;
+import com.posomo.saltit.domain.restaurant.dto.RestaurantSummaryResponse;
+import com.posomo.saltit.domain.restaurant.entity.Restaurant;
+import com.posomo.saltit.repository.RestaurantRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+// @Service
+@RequiredArgsConstructor
+@Transactional
+public class RestaurantServiceV2 implements RestaurantService {
+	private final RestaurantRepository restaurantRepository;
+
+	@Override
+	public RestaurantSummaryResponse getRestaurantSummaries(RestaurantFilterRequest filterRequest) {
+		List<RestaurantSummary> restaurant = restaurantRepository.findRestaurantBy(filterRequest);
+		Slice<Object[]> resultObjects = restaurantRepository.findRestaurantByFilter(
+			filterRequest.getMaxPrice(),
+			filterRequest.getFoodTypeName(),
+			filterRequest.computeMySqlPoint(),
+			filterRequest.getMaxDistance(),
+			filterRequest.createPageRequest()
+		);
+		return RestaurantSummaryResponse.of(resultObjects);
+	}
+
+	@Override
+	public RestaurantDetailResponse getRestaurantDetail(long restaurantId) {
+		Restaurant restaurant = restaurantRepository.findByIdWithMenus(restaurantId).orElseThrow(NoRecordException::new);
+		return RestaurantDetailResponse.of(restaurant);
+	}
+}
