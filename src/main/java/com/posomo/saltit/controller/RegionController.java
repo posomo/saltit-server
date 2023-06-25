c9d243aae7c550f4b937e2fb9c7fc12643b6da1f
jhbaik1501 <81180977+jhbaik1501@users.noreply.github.com>
2023-06-25 22:49:35 +0900

    Update RegionController.java

--- src/main/java/com/posomo/saltit/controller/RegionController.java
+++ src/main/java/com/posomo/saltit/controller/RegionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.posomo.saltit.domain.region.dto.RegionListResponseDto;
 
 @RequestMapping("/api/v1/region")
-public interface RegionController {
+public interface RegionController { 
 	@GetMapping("/list")
 	RegionListResponseDto getRegionList();
 }
